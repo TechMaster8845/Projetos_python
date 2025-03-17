d73e3474f54d4451953addd5f1d6860d3d29e24e
TechMaster8845 <techmaster.8845@gmail.com>
2025-03-17 12:34:33 -0300

    Subindo exercícios do ambiente venv

---
+++ saudacao.py
@@ -0,0 +1,6 @@
+def main():
+    nome = input("Escreva seu nome completo! ")
+    print(f"A paz de Deus! {nome} que Deus abençoe seu dia.")
+
+if __name__ == "__main__":
+    main()
---
+++ soma.py
@@ -0,0 +1,11 @@
+def soma(a, b):
+    return(a+b)
+
+def main():
+    x = int(input("Digite o primeiro numero: "))
+    y = int(input("Digite o segundo numero: "))
+    print(f"A soma é {soma(x, y)}")
+
+
+if __name__ == "__main__":
+    main()
