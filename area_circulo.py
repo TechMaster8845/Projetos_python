0b2b951f7a0762c56d1a5e3cb650a64e9d2287a5
TechMaster8845 <techmaster.8845@gmail.com>
2025-03-19 05:53:29 -0300

    Atualizando area_circulo.py

--- area_circulo.py
+++ area_circulo.py
@@ -1,12 +1,16 @@
-import math
+import math #Chamando a biblioteca math
+class CalculoArea:
+    def __init__(self,raio):
+        self.raio = raio #Armazena o raio dentro do objeto
 
-def area_circulo(raio):
-    return math.pi * (raio ** 2)
+    def area_circulo(self):
+        return math.pi * (raio ** 2)
 
 
 def main():
     r = float(input("Digite o raio do circulo: "))
-    area = area_circulo(r)
+    calculo = CalculoArea(r) #criando um objeto de classe
+    area = calculo.area_circulo()  #Chamando o método da classe
     print(f"A area do circulo é {area:.2f}m²")
 
 
